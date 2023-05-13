47b8749ffb897526903c16c6641fc1cc590a86b9
Dimas-AnjayMabar <sandipratama164026@gmail.com>
2023-05-13 14:35:13 +0700

    commit ke 30

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
-var typed = new Typed('.typing', {
-  strings: ["Toko berbagai macam alat dan bahan kue.", "Surga Alat dan Bahan Kue di Jombang."],
+var typed = new Typed(".typing", {
+  strings: [
+    "Toko berbagai macam alat dan bahan kue.",
+    "Surga Alat dan Bahan Kue di Jombang.",
+  ],
   typeSpeed: 100,
   delaySpeed: 300,
   loop: true,
+});
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyP0dsDXI8N0-I6fueNe2KZxF4Gh2rJkSNKo436GTgynUXTuyEoiL7Tg5CRWtJd6fn-YA/exec";
+const form = document.forms["TOBAKU-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika tombol submit diklik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
